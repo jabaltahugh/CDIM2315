6b7b08983509ea48b543cda4db27438da2edede9
jabaltahugh <157274146+jabaltahugh@users.noreply.github.com>
2024-01-29 22:30:33 -0600

    Homework2Q1

---
+++ Homework2Q1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please input a letter grade: ");
+        string grade = Console.ReadLine().ToUpper();
+        switch (grade)
+        { 
+            case 'A':
+                gpaPoints = 4;
+                break;
+            case 'B':
+                gpaPoints = 3;
+                break;
+            case 'C':
+                gpaPoints = 2;
+                break;
+            case 'D':
+                gpaPoints = 1;
+                break;
+            case 'F':
+                gpaPoints = 0;
+                break;
+            default:
+                Console.WriteLine("Wrong Letter Grade!");
+                break;
+        }
+
+        Console.WriteLine($"GPA Points: {gpaPoints}");
+    }
+}    
+        
